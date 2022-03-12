ca7d11523a4001f8436042c8100e63ed777f4819
sumryooo <adha.surya2002@gmail.com>
2022-03-12 17:10:04 +0700

    posisiKarakterGame

---
+++ posisiKarakterGame.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PraktikumMod3_1302204026
+{
+    internal class posisiKarakterGame
+    {
+    }
+}
